1257989e4a049ca7d154a0fbd853a925a0020c69
agreene <agreene@teksystems.com>
2017-01-31 16:07:36 -0500

    More Routing and Browser History

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,7 +1,7 @@
 'use strict';
 
 eventsApp.factory('eventData', function ($resource) {
-    var resource = $resource('/data/event/:id', { id: '@id' });
+    var resource = $resource('/data/event/:id', { id: '@id' }, {"getAll": {method: "GET", isArray: true}});
     return {
         getEvent: function () {
             return resource.get({ id: 1 });
@@ -9,6 +9,9 @@ eventsApp.factory('eventData', function ($resource) {
         save: function(event) {
             event.id = 999;
             return resource.save(event);
+        },
+        getAllEvents: function() {
+            return resource.query();
         }
     };
 });
