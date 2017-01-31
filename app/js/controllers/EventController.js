5209ddb5035bc9320b93b789a5d3f405f37d1b7e
agreene <agreene@teksystems.com>
2017-01-31 16:24:40 -0500

    Accessing Parameters from the Route

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,14 +1,10 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope, eventData, $anchorScroll) {
+    function EventController($scope, eventData, $routeParams) {
         $scope.sortorder = 'name';
 
-        eventData.getEvent()
-            .$promise
-            .then(function (event) { $scope.event = event; })
-            .catch(function (response) { console.log(response); }
-            );
+        $scope.event = eventData.getEvent($routeParams.eventId);
 
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
@@ -16,9 +12,5 @@ eventsApp.controller('EventController',
 
         $scope.downVoteSession = function (session) {
             session.upVoteCount--;
-        }
-
-        $scope.scrollToSession = function() {
-            $anchorScroll();
         }
     });
